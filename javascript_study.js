ffa8f177bcf7b739fb9beb363fd079711fe0cafa
Junhyeok-Lim <junh0301@naver.com>
2023-04-14 10:35:25 +0900

    javascript_start

---
+++ javascript_study.js
@@ -0,0 +1,24 @@
+// let name = "Mike"; //변할 수 있는 값
+
+
+// const Pi = 3.141592;// 절대로 바뀌지 않는 상수 선언, 가급적 대문자로 선언
+
+
+// const message3 = 'My name is ${name}';
+// const message4 = '나는 ${30+1}살 입니다.';
+// console.log(message3);
+// console.log(message4);
+
+// const age = 30;
+// const PI = 3.14;
+
+// console.log(1+2);
+// console.log(10-3);
+// console.log(3*2);
+// console.log(6/3);
+// console.log(6%4);
+
+// const name = prompt("이름을 입력하세요.");
+// alert("환영합니다, " + name + "님");
+
+var num = 10;
